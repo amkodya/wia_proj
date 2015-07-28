8e455dcba2e382fca9bfbe2ae6ef27f387158970
amkodya <amkodya@fullsail.edu>
2015-07-27 21:03:29 -0400

    lightbox 1st run
    lighbox 1st run

---
+++ js/lbox.js
@@ -0,0 +1,42 @@
+window.onload = function() {
+	
+	$(".lightbox_trigger").click(function(e){
+
+
+		//prevent default action (make picture not clickable)
+		e.preventDefault();
+
+		//get clicked links href
+		var image_href = $(this).attr("href");
+		console.log(image_href);
+
+		//setup the lighbox if not already setup or usethe lightbox if it exists
+
+		if($("#lightbox").length>0){
+			//lightbox div is already created = we can use it
+
+			//place the clicked href into our lightbox div
+			$("#content").html('<img src="' + image_href + '" />');
+
+		} else {
+			//lightbox div will not exist 1st time
+
+			var lightbox = 
+				'<div id="lightbox">' +
+				'<p>Click to Close</p>' +
+				'<div id="content">' +
+				//insert the clicked link href into an image
+				'<img src="' + image_href + '" />' +
+				'</div>' + //ends content div
+				'</div>' //closes lightbox div
+
+				//insert the lightbox html into our page
+				$("body").append(lightbox);
+
+
+
+		}
+
+
+	});
+}
